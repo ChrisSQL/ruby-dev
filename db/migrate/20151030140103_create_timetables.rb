8031d8a3b3cbe7c731fba22d90908deed81160ad
Chris Maher <chrismaher.wit@gmail.com>
2015-11-09 16:15:38 +0000

    Schema Fix.

--- db/migrate/20151030140103_create_timetables.rb
+++ db/migrate/20151030140103_create_timetables.rb
@@ -3,7 +3,6 @@ class CreateTimetables < ActiveRecord::Migration
     create_table :timetables do |t|
       t.string :name
       t.string :attachment
-      t.integer :user_id
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/schemaXXX.rb
+++ db/schemaXXX.rb
@@ -3,7 +3,7 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
+# Note that this schemaXXX.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
@@ -110,5 +110,4 @@ ActiveRecord::Schema.define(version: 20151103175345) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   add_foreign_key "projects", "users"
-  add_foreign_key "timetables", "users"
 end
